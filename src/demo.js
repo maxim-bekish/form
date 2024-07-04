611857ced6a169e01be0b20a6233fd5fa2fef132
Maxim_belish <maxamax997@gmail.com>
2024-07-04 14:25:56 +0300

    fix path for git hub Pages

--- src/demo.js
+++ src/demo.js
@@ -2,7 +2,7 @@ import { getLocal } from './helper.js';
 
 let currentUrl = window.location.pathname;
 const showLogin = document.getElementById("showLogin");
-if (currentUrl === '/src/demo.html') {
+if (currentUrl === '/src/demo.html' || currentUrl === "/form/src/demo.html") {
    showLogin.textContent = `Ваш логин ${getLocal('user')}. `;
 }
 
