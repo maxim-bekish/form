5b817122c77f073cdc6fb89183eb2b390855a072
Maxim_belish <maxamax997@gmail.com>
2024-07-04 14:31:57 +0300

    fix

--- src/demo.js
+++ src/demo.js
@@ -2,6 +2,7 @@ import { getLocal } from './helper.js';
 
 let currentUrl = window.location.pathname;
 const showLogin = document.getElementById("showLogin");
+console.log(currentUrl)
 if (currentUrl === '/src/demo.html' || currentUrl === "/form/src/demo.html") {
    showLogin.textContent = `Ваш логин ${getLocal('user')}. `;
 }
